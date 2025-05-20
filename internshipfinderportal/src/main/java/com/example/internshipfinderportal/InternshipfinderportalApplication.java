f5cfb116f73e6e349094551ceb45c42b6c9c5ba5
SHERATONS <act22970@gmail.com>
2025-05-20 18:00:58 +0700

    setup project and database(postgresql)

---
+++ internshipfinderportal/src/main/java/com/example/internshipfinderportal/InternshipfinderportalApplication.java
@@ -0,0 +1,13 @@
+package com.example.internshipfinderportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipfinderportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipfinderportalApplication.class, args);
+	}
+
+}
---
+++ internshipfinderportal/src/test/java/com/example/internshipfinderportal/InternshipfinderportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internshipfinderportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipfinderportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
